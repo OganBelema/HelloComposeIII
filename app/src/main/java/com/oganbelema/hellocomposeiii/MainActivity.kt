2390eb8d0829dfb1df729ea22da697d65988713f
Oganbelema <oganbelema@gmail.com>
2022-08-17 12:51:29 +0100

    added text widgets to topheader and updatdd background color

--- app/src/main/java/com/oganbelema/hellocomposeiii/MainActivity.kt
+++ app/src/main/java/com/oganbelema/hellocomposeiii/MainActivity.kt
@@ -3,18 +3,18 @@ package com.oganbelema.hellocomposeiii
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.oganbelema.hellocomposeiii.ui.theme.HelloComposeIIITheme
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloComposeIIITheme {
                 MyApp {
-                    Text(text = "Hello World!")
+                    TopHeader()
                 }
             }
         }
@@ -36,7 +36,6 @@ class MainActivity : ComponentActivity() {
 fun MyApp(content: @Composable () -> Unit){
     // A surface container using the 'background' color from the theme
     Surface(
-        modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
         content()
@@ -45,13 +44,24 @@ fun MyApp(content: @Composable () -> Unit){
 
 @Preview
 @Composable
-fun TopHeader() {
-    Surface(modifier = Modifier.fillMaxWidth()
+fun TopHeader(totalPerPerson: Double = 0.0) {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
         .height(150.dp)
         .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
-    color = Color.Magenta
+    color = Color(0xFF64B5F6)
     ) {
+        val total = "%.2f".format(totalPerPerson)
 
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Total Per Person", style = MaterialTheme.typography.h5)
+            Text(text = "€$total", style = MaterialTheme.typography.h4,
+            fontWeight = FontWeight.ExtraBold)
+        }
     }
 
 }
@@ -61,7 +71,7 @@ fun TopHeader() {
 fun DefaultPreview() {
     HelloComposeIIITheme {
         MyApp {
-            Text(text = "Hello World!")
+            TopHeader()
         }
     }
 }

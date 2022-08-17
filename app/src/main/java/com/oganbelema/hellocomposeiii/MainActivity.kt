b5ef834f7782009d55ff408b841cc2b39f45edf3
Oganbelema <oganbelema@gmail.com>
2022-08-17 20:42:33 +0100

    added bill amount call back to get input data from textfield

--- app/src/main/java/com/oganbelema/hellocomposeiii/MainActivity.kt
+++ app/src/main/java/com/oganbelema/hellocomposeiii/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.oganbelema.hellocomposeiii
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -10,11 +11,9 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -84,6 +83,18 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
 @ExperimentalComposeUiApi
 @Composable
 fun MainContent() {
+
+    BillForm() { billAmount ->
+        Log.d("Callback", billAmount)
+    }
+
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun BillForm(modifier: Modifier = Modifier,
+onValChange: (String) -> Unit = {}){
+
     val totalBillState = remember {
         mutableStateOf("")
     }
@@ -106,12 +117,13 @@ fun MainContent() {
                 labelId = "Enter Bill",
                 enabled = true,
                 isSingleLine = true,
-            onAction = KeyboardActions {
-                if (!validState) return@KeyboardActions
-                //Todo - onvaluechanged
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
 
-                keyboardController?.hide()
-            })
+                    onValChange(totalBillState.value.trim())
+
+                    keyboardController?.hide()
+                })
         }
     }
 }

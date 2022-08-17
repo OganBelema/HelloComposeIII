0f896350945bc5ff387f89ae571c707aaa6342a6
Oganbelema <oganbelema@gmail.com>
2022-08-17 19:06:05 +0100

    added outlined textfield to main content

--- app/src/main/java/com/oganbelema/hellocomposeiii/MainActivity.kt
+++ app/src/main/java/com/oganbelema/hellocomposeiii/MainActivity.kt
@@ -8,27 +8,36 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ExperimentalComposeApi
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.oganbelema.hellocomposeiii.components.InputField
 import com.oganbelema.hellocomposeiii.ui.theme.HelloComposeIIITheme
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HelloComposeIIITheme {
                 MyApp {
-                    TopHeader()
+                    //TopHeader()
+                    MainContent()
                 }
             }
         }
@@ -72,8 +81,19 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
 }
 
 @Preview
+@ExperimentalComposeUiApi
 @Composable
 fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Surface(
         modifier = Modifier
             .padding(2.dp)
@@ -82,7 +102,16 @@ fun MainContent() {
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
         Column() {
+            InputField(valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+            onAction = KeyboardActions {
+                if (!validState) return@KeyboardActions
+                //Todo - onvaluechanged
 
+                keyboardController?.hide()
+            })
         }
     }
 }
